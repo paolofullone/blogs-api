7672cf538a4693be7811720b6c83aae1d4528d2c
paolofullone <paolo.enrico@gmail.com>
2022-06-12 06:02:04 -0300

    refactor: :art: including the id in user model as per req02

--- src/database/models/user.js
+++ src/database/models/user.js
@@ -2,6 +2,7 @@
 
 const UserSchema = (sequelize, DataTypes) => {
   const UserTable = sequelize.define("User", {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
     displayName: DataTypes.STRING,
     email: DataTypes.STRING,
     password: DataTypes.STRING,
