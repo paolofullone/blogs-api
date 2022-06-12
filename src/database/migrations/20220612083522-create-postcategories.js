c67a428c11abf1213397ebdec87b6148505e4d59
paolofullone <paolo.enrico@gmail.com>
2022-06-12 19:56:19 -0300

    feat(Sequelize Config): :sparkles: postCategories

--- src/database/migrations/20220612083522-create-postcategories.js
+++ src/database/migrations/20220612083522-create-postcategories.js
@@ -11,7 +11,7 @@ module.exports = {
         },
         onDelete: 'CASCADE',
         primaryKey: true,
-      },
+      },    
       categoryId: {
         type: Sequelize.INTEGER,
         references: {
