c31c2a1d9410f91c73ba7ee513970785d423ac0d
paolofullone <paolo.enrico@gmail.com>
2022-06-13 11:08:09 -0300

    feat(ExpressConfigs): :sparkles: Route /categories validated

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const userController = require('../controllers/userController');
+const categoryController = require('../controllers/categoryController');
+const validateAuth = require('../middlewares/validateAuth');
 
 const routers = express.Router();
 
 routers.use('/', userController);
+routers.use('/categories', validateAuth, categoryController);
 
 module.exports = routers;
