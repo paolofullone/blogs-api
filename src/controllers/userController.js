0e960a6a4ea22c37942ef5ba997c8512bca35540
paolofullone <paolo.enrico@gmail.com>
2022-06-13 08:39:25 -0300

    feat(MSC Layers): :sparkles: getUser in UserController

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,19 +1,25 @@
 const express = require('express');
 
 const loginRouter = express.Router();
-const loginService = require('../services/userService');
+const userService = require('../services/userService');
 const validateLogin = require('../middlewares/validateLogin');
 const validateUser = require('../middlewares/validateUser');
+const authenticateMiddleware = require('../middlewares/auth');
 
 loginRouter.post('/login', validateLogin, async (req, res) => {
     const { email, password } = req.body;
-    const token = await loginService.getUserByEmail(email, password);
+    const token = await userService.getUserByEmail(email, password);
     res.send({ token });
 });
 
 loginRouter.post('/user', validateUser, async (req, res) => { 
-    const token = await loginService.createUser(req.body);
+    const token = await userService.createUser(req.body);
     res.status(201).send({ token });
+});
+
+loginRouter.get('/user', authenticateMiddleware, async (req, res) => {
+    const users = await userService.getUsers();
+    res.send(users);
 });
 
 module.exports = loginRouter;
