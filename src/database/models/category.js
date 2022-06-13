f0480e089a1f641da053c750320fa71b523379dc
paolofullone <paolo.enrico@gmail.com>
2022-06-13 09:22:08 -0300

    fix(MSC Layers): :bug: including id in categoryModel

--- src/database/models/category.js
+++ src/database/models/category.js
@@ -2,6 +2,11 @@
 
 const CategoriesSchema = (sequelize, DataTypes) => {
   const CategoriesTable = sequelize.define("Category", {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
     name: DataTypes.STRING,
   }, {
     timestamps: false,
