0b64b36ea286987f62fe3bad3b45fc01b23e17bd
paolofullone <paolo.enrico@gmail.com>
2022-06-13 08:40:17 -0300

    feat(MSC Layers): :sparkles: userAuth middleware

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,14 +1,8 @@
 const { authenticateToken } = require('../utils/jwt');
 
 const authenticateMiddleware = async (req, res, next) => {
-    console.log('entrou na authenticateMiddleware');
     const token = req.headers.authorization;
-    // console.log(token);
     const user = await authenticateToken(token);
-    if (!user) {
-        const error = { status: 401, message: 'JWT malformed' };
-        throw error;
-    }
     res.locals.user = user;
     next();
 };
