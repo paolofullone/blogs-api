f5cff347f58557c23bcd9cc5c5ffab78c6fa4100
paolofullone <paolo.enrico@gmail.com>
2022-06-14 09:14:08 -0300

    feat(MSC Layers): :sparkles: get all posts services

--- src/services/blogPostService.js
+++ src/services/blogPostService.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 const { BlogPost } = require('../database/models');
 const { sequelize } = require('../database/models');
-const { User } = require('../database/models');
+const { User, Category } = require('../database/models');
 
 const getUser = async (user) => {
   const result = jwt.verify(user, process.env.JWT_SECRET);
@@ -14,13 +14,13 @@ const createBlogPost = async (user, { title, content, categoryIds }) => {
   try {
     const userSearchedId = await getUser(user);
     const result = await sequelize.transaction(async (t) => {
-        const blogPost = await BlogPost.create({
-            title,
-            content,
-            userId: userSearchedId,
-            published: new Date(),
-            updated: new Date(),
-        }, { transaction: t });
+      const blogPost = await BlogPost.create({
+        title,
+        content,
+        userId: userSearchedId,
+        published: new Date(),
+        updated: new Date(),
+      }, { transaction: t });
       await blogPost.addCategories(categoryIds, { transaction: t });
       return blogPost;
     });
@@ -31,6 +31,19 @@ const createBlogPost = async (user, { title, content, categoryIds }) => {
   }
 };
 
+const getAllBlogPosts = async () => {
+  console.log('entrou na blogPostService');
+  const blogPosts = await BlogPost.findAll({
+    include: [
+      { model: User, as: 'user', attributes: { exclude: ['password'] } },
+      { model: Category, as: 'categories', through: { attributes: [] } },
+    ],
+  });
+  console.log('blogPosts', blogPosts);
+  return blogPosts;
+};
+
 module.exports = {
   createBlogPost,
+  getAllBlogPosts,
 };
