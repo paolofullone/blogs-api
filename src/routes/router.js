690907f37eb33126c2b6dec4b9f15a4ed2fe1f7c
paolofullone <paolo.enrico@gmail.com>
2022-06-14 00:00:49 -0300

    feat(ExpressConfigs): :sparkles: blogPost router

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const userController = require('../controllers/userController');
 const categoryController = require('../controllers/categoryController');
+const blogPostController = require('../controllers/blogPostController');
 const validateAuth = require('../middlewares/validateAuth');
+const validatePost = require('../middlewares/validatePost');
 
 const routers = express.Router();
 
 routers.use('/', userController);
 routers.use('/categories', validateAuth, categoryController);
+routers.use('/post', validateAuth, validatePost, blogPostController);
 
 module.exports = routers;
