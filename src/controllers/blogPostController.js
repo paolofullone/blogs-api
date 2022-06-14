d28ba2abd723f01b2605597149a7b5150f182c05
paolofullone <paolo.enrico@gmail.com>
2022-06-14 09:12:45 -0300

    feat(MSC Layers): :sparkles: get all posts controller

--- src/controllers/blogPostController.js
+++ src/controllers/blogPostController.js
@@ -2,14 +2,20 @@ const express = require('express');
 
 const blogPostService = require('../services/blogPostService');
 const userService = require('../services/userService');
+const validatePost = require('../middlewares/validatePost');
 
 const blogPostRouter = express.Router();
 
-blogPostRouter.post('/', async (req, res) => {
+blogPostRouter.post('/', validatePost, async (req, res) => {
     const { email, password } = res.user;
     const user = await userService.getUserByEmail(email, password);
     const newPost = await blogPostService.createBlogPost(user, req.body);
     res.status(201).send(newPost);
+});
+
+blogPostRouter.get('/', async (req, res) => {
+    const posts = await blogPostService.getAllBlogPosts();
+    res.status(200).send(posts);
 });
 
 module.exports = blogPostRouter;
