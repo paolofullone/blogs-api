a98c143bac352864417219b19040c22d9b1a9dba
paolofullone <paolo.enrico@gmail.com>
2022-06-14 09:12:00 -0300

    fix(ExpressConfigs): :bug: Remove validatePost from general route

--- src/routes/router.js
+++ src/routes/router.js
@@ -3,12 +3,11 @@ const userController = require('../controllers/userController');
 const categoryController = require('../controllers/categoryController');
 const blogPostController = require('../controllers/blogPostController');
 const validateAuth = require('../middlewares/validateAuth');
-const validatePost = require('../middlewares/validatePost');
 
 const routers = express.Router();
 
 routers.use('/', userController);
 routers.use('/categories', validateAuth, categoryController);
-routers.use('/post', validateAuth, validatePost, blogPostController);
+routers.use('/post', validateAuth, blogPostController);
 
 module.exports = routers;
