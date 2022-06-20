40a3041d5a448daf046e4929832c00b88274dfce
paolofullone <paolo.enrico@gmail.com>
2022-06-20 10:52:22 -0300

    refactor: :art: userController w/o login

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,33 +2,26 @@ const express = require('express');
 
 const userRouter = express.Router();
 const userService = require('../services/userService');
-const validateLogin = require('../middlewares/validateLogin');
 const validateUser = require('../middlewares/validateUser');
 const validateAuth = require('../middlewares/validateAuth');
 
-userRouter.post('/login', validateLogin, async (req, res) => {
-    const { email, password } = req.body;
-    const token = await userService.getUserByEmail(email, password);
-    res.send({ token });
-});
-
-userRouter.post('/user', validateUser, async (req, res) => { 
+userRouter.post('/', validateUser, async (req, res) => { 
     const token = await userService.createUser(req.body);
     res.status(201).send({ token });
 });
 
-userRouter.get('/user', validateAuth, async (_req, res) => {
+userRouter.get('/', validateAuth, async (_req, res) => {
     const users = await userService.getUsers();
     res.send(users);
 });
 
-userRouter.get('/user/:id', validateAuth, async (req, res) => {
+userRouter.get('/:id', validateAuth, async (req, res) => {
     const { id } = req.params;
     const user = await userService.getUserById(id);
     res.send(user);
 });
 
-userRouter.delete('/user/me', validateAuth, async (_req, res) => {
+userRouter.delete('/me', validateAuth, async (_req, res) => {
     const { user } = res;
     await userService.deleteUser(user);
     res.status(204).send();
