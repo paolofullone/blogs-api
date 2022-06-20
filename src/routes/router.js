ffb306fa396adc32c3539c5429adcbc85dc5ca1b
paolofullone <paolo.enrico@gmail.com>
2022-06-20 10:53:11 -0300

    refactor(Overall): :art: routes to accommodate login controller

--- src/routes/router.js
+++ src/routes/router.js
@@ -2,11 +2,16 @@ const express = require('express');
 const userController = require('../controllers/userController');
 const categoryController = require('../controllers/categoryController');
 const blogPostController = require('../controllers/blogPostController');
+const loginController = require('../controllers/loginController');
 const validateAuth = require('../middlewares/validateAuth');
 
 const routers = express.Router();
 
-routers.use('/', userController);
+// criar uma arquivo loginController e chamar aqui, mudar o userController para /user
+routers.use('/login', loginController);
+
+routers.use('/user', userController);
+
 routers.use('/categories', validateAuth, categoryController);
 routers.use('/post', validateAuth, blogPostController);
 
