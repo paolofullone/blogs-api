f93f7acfd8ba0aabef2d3af5347184c87d0e5240
paolofullone <paolo.enrico@gmail.com>
2022-06-24 20:16:15 -0300

    feat: :sparkles: bcrypt (commented)
    crashed the tests, but bcrypt configured successfully

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,23 +1,30 @@
+// const bcrypt = require('bcrypt');
 const { User } = require('../database/models');
 const { getUserId } = require('../utils/getUserId');
 const { generateJWTToken } = require('../utils/jwt');
+// const { checkUser } = require('../utils/checkUser');
 
+// const getUserByEmail = async (email, password) => {
 const getUserByEmail = async (email) => {
     const user = await User.findOne({ where: { email } });
     if (!user) {
         const error = { status: 400, message: 'Invalid fields' };
         throw error;
     }
+    // return checkUser(email, password, user.password);
     return generateJWTToken(email);
 };
-
 const createUser = async ({ displayName, email, password, image }) => {
     const UserExists = await User.findOne({ where: { email } });
     if (UserExists) {
         const error = { status: 409, message: 'User already registered' };
         throw error;
     }
+    // const salt = await bcrypt.genSalt(10);
+    // const hash = await bcrypt.hash(password, salt);
+    // await User.create({ displayName, email, password: hash, image });
     await User.create({ displayName, email, password, image });
+
     return generateJWTToken(email);
 };
 
