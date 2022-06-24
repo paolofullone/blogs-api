2d7ede9fd7c833e2a5f962d4b15fd186abdb5b43
paolofullone <paolo.enrico@gmail.com>
2022-06-24 08:01:52 -0300

    refactor: :fire: remove unnecessary line

--- src/middlewares/validateAuth.js
+++ src/middlewares/validateAuth.js
@@ -2,7 +2,6 @@ const { authenticateToken } = require('../utils/jwt');
 
 const validateAuth = async (req, res, next) => {
     const token = req.headers.authorization;
-    await authenticateToken(token);
     const user = await authenticateToken(token);
     res.user = user;
     next();
