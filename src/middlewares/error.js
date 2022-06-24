d6fcd76b289ae0b16045d4d8c7094799592f06e6
paolofullone <paolo.enrico@gmail.com>
2022-06-24 20:13:46 -0300

    refactor: :art: unused req added _

--- src/middlewares/error.js
+++ src/middlewares/error.js
@@ -1,4 +1,4 @@
-module.exports = (err, req, res, _next) =>
+module.exports = (err, _req, res, _next) =>
     res.status(err.status || 500).json(
         { message: err.message || 'Erro inesperado. Por favor, tente mais tarde' },
     );
