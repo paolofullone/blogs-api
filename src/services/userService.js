9754b18fa2c4f4fe2605222675c22049358f6f16
paolofullone <paolo.enrico@gmail.com>
2022-06-26 17:25:04 -0300

    feat: :sparkles: user Service to includes bcrypt

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,18 +1,16 @@
-// const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt');
 const { User } = require('../database/models');
 const { getUserId } = require('../utils/getUserId');
 const { generateJWTToken } = require('../utils/jwt');
-// const { checkUser } = require('../utils/checkUser');
+const { checkUser } = require('../utils/checkUser');
 
-// const getUserByEmail = async (email, password) => {
-const getUserByEmail = async (email) => {
+const getUserByEmail = async (email, password) => {
     const user = await User.findOne({ where: { email } });
     if (!user) {
         const error = { status: 400, message: 'Invalid fields' };
         throw error;
     }
-    // return checkUser(email, password, user.password);
-    return generateJWTToken(email);
+    return checkUser(email, password, user.password);
 };
 const createUser = async ({ displayName, email, password, image }) => {
     const UserExists = await User.findOne({ where: { email } });
@@ -20,11 +18,9 @@ const createUser = async ({ displayName, email, password, image }) => {
         const error = { status: 409, message: 'User already registered' };
         throw error;
     }
-    // const salt = await bcrypt.genSalt(10);
-    // const hash = await bcrypt.hash(password, salt);
-    // await User.create({ displayName, email, password: hash, image });
-    await User.create({ displayName, email, password, image });
-
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
+    await User.create({ displayName, email, password: hash, image });
     return generateJWTToken(email);
 };
 
